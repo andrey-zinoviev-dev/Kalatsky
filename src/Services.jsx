b4b51f7c1c0c2742c0b2e2e44eae2a9cf53eb2d0
Andrey Zinoviev <dronis_dronis@mail.ru>
2023-12-26 02:46:58 +0300

    added initial content to landing component and services

--- src/Services.jsx
+++ src/Services.jsx
@@ -1,13 +1,21 @@
 import { services } from "./utils";
+import "./Services.css";
 
 export default function Services() {
   return (
-    <section>
-      <ul>
-        {services.map((service) => {
-          return <li key={service.name}><p>{service.name}</p></li>
-        })}
-      </ul>
+    <section className="services">
+      <div className="container">
+        <h2>Услуги</h2>
+        <ul className="services__ul">
+          {services.map((service) => {
+            return <li className="services__ul-li" key={service.name}>
+              <button>
+                <p>{service.name}</p>
+              </button>
+            </li>
+          })}
+        </ul>
+      </div>
     </section>
   )
 }
