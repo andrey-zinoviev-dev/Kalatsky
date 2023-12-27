585c33637e134e85af50003ecfa0f54d106d77fe
Andrey Zinoviev <dronis_dronis@mail.ru>
2023-12-27 20:48:44 +0300

    added contant form

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,28 +1,40 @@
+import "./Form.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPhone, faEnvelope, faLocationDot } from "@fortawesome/free-solid-svg-icons";
+
 export default function Form() {
   return (
-    <section>
+    <section className="contanct">
       <div className="container">
         <h2>Связаться со мной</h2>
-        <ul>
+        <ul className="contanct__ul">
           <li>
-            <a href="tel:+79588280774">+7 958 828 07 74</a>
+            <FontAwesomeIcon icon={faPhone} />
+            <a href="tel:+79588280774">+79857686442</a>
           </li>
           <li>
-            <a href="mailto:sttrog_810@mail.ru">sttog_810@mail.ru</a>
+            <FontAwesomeIcon icon={faEnvelope} />
+            <a href="mailto:sttrog_810@mail.ru">law@kalatsky.com</a>
           </li>
           <li>
+            <FontAwesomeIcon icon={faLocationDot} />
             <p>Россия, Москва, Климашкина, 9-63</p>
           </li>
         </ul>
-        <form>
-          <input placeholder="Имя" type="text"></input>
-          <input placeholder="Фамилия" type="text"></input>
-          <input placeholder="Email" type="email"></input>
-          <div>
+        <form className="contanct__form">
+          <div className="contanct__form-div">
+            <input placeholder="Имя" type="text"></input>
+            <input placeholder="Фамилия" type="text"></input>
+          </div>
+          <div className="contanct__form-div">
+            <input placeholder="Email" type="email"></input>
+            <input placeholder="Телефон" type="phone"></input>
+          </div>
+          <div className="contanct__form-message">
             <label>Опишите Вашу ситуацию</label>
             <textarea></textarea>
           </div>
-          <button type="button">Отправить</button>
+          <button className="contanct__form-btn" type="button">Отправить</button>
         </form>
       </div>
     </section>
