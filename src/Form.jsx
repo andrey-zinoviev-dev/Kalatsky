7cf0ba6dec783fd48f21cddd12a468b980646fef
Andrey Zinoviev <dronis_dronis@mail.ru>
2023-12-29 02:59:37 +0300

    updated contact section

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,41 +1,52 @@
+import React from "react";
 import "./Form.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPhone, faEnvelope, faLocationDot } from "@fortawesome/free-solid-svg-icons";
+import map from "./assets/map.png";
 
-export default function Form() {
+export default function Form({orderClicked}) {
+  const sectionRef = React.useRef();
+
+  React.useEffect(() => {
+    orderClicked && sectionRef.current.scrollIntoView({behavior: "smooth", block: "center"});
+  }, [orderClicked]);
+
   return (
-    <section className="contanct">
+    <section className="contanct" ref={sectionRef}>
       <div className="container">
         <h2>Связаться со мной</h2>
         <ul className="contanct__ul">
-          <li>
-            <FontAwesomeIcon icon={faPhone} />
-            <a href="tel:+79588280774">+79857686442</a>
-          </li>
-          <li>
-            <FontAwesomeIcon icon={faEnvelope} />
-            <a href="mailto:sttrog_810@mail.ru">law@kalatsky.com</a>
-          </li>
-          <li>
-            <FontAwesomeIcon icon={faLocationDot} />
-            <p>Россия, Москва, Климашкина, 9-63</p>
-          </li>
+              <li>
+                <FontAwesomeIcon icon={faPhone} />
+                <a href="tel:+79588280774">+79857686442</a>
+              </li>
+              <li>
+                <FontAwesomeIcon icon={faEnvelope} />
+                <a href="mailto:sttrog_810@mail.ru">law@kalatsky.com</a>
+              </li>
+              <li>
+                <FontAwesomeIcon icon={faLocationDot} />
+                <p>Россия, Москва, Климашкина, 9-63</p>
+              </li>
         </ul>
-        <form className="contanct__form">
-          <div className="contanct__form-div">
-            <input placeholder="Имя" type="text"></input>
-            <input placeholder="Фамилия" type="text"></input>
-          </div>
-          <div className="contanct__form-div">
-            <input placeholder="Email" type="email"></input>
-            <input placeholder="Телефон" type="phone"></input>
-          </div>
-          <div className="contanct__form-message">
-            <label>Опишите Вашу ситуацию</label>
-            <textarea></textarea>
-          </div>
-          <button className="contanct__form-btn" type="button">Отправить</button>
-        </form>
+        <div className="contanct__wrapper">
+          <form className="contanct__form">
+            <div className="contanct__form-div">
+              <input placeholder="Имя" type="text"></input>
+              <input placeholder="Фамилия" type="text"></input>
+            </div>
+            <div className="contanct__form-div">
+              <input placeholder="Email" type="email"></input>
+              <input placeholder="Телефон" type="phone"></input>
+            </div>
+            <div className="contanct__form-message">
+              <label>Опишите Вашу ситуацию</label>
+              <textarea></textarea>
+            </div>
+            <button className="contanct__form-btn" type="button">Отправить</button>
+          </form>
+          <img className="contanct__wrapper-img" src={map} />
+        </div>
       </div>
     </section>
   )
--- src/Landing.jsx
+++ src/Landing.jsx
@@ -1,9 +1,16 @@
+import React from 'react';
 import Header from './Header';
 import Services from './Services'
 import Form from './Form';
 import "./Landing.css";
 // import Pic from "./assets/AK6A4769.jpg"
 export default function Landing() {
+  const [orderClicked, setOrderClicked] = React.useState(false);
+  React.useEffect(() => {
+    orderClicked &&     setTimeout(() => {
+      setOrderClicked(false);
+    }, 1000)
+  }, [orderClicked])
   return (
     <main>
       <Header />
@@ -12,14 +19,16 @@ export default function Landing() {
           <div>
             <h1>Юридические консультации</h1>
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos nihil voluptatem perspiciatis dolorem aperiam aliquid totam dicta suscipit, iusto, illo quasi, maiores vel quo dolorum accusamus modi optio odit magni</p>
-            <button>Заказать услугу</button>
+            <button onClick={() => {
+              setOrderClicked(true);
+            }}>Заказать услугу</button>
           </div>
           
         </div>
         {/* <img src={Pic} alt="" /> */}
       </section>
       <Services />
-      <Form />
+      <Form orderClicked={orderClicked} />
     </main>
   )
 }
