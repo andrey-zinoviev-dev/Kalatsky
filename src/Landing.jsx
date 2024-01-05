226bcfd22f69842ece49ae7860cfef8ddb52ed07
Andrey Zinoviev <dronis_dronis@mail.ru>
2024-01-06 01:14:37 +0300

    updated bg in landing section

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -4,6 +4,7 @@ import Services from './Services'
 import Form from './Form';
 import "./Landing.css";
 import Navigation from './Navigation';
+// import Pic from "./assets/bg-white.png";
 // import Pic from "./assets/AK6A4769.jpg"
 // import Pic from "./assets/AK6A4769.png";
 // import Pic from "./assets/bg-new.png";
@@ -29,8 +30,8 @@ export default function Landing({navOpened, setNavOpened}) {
           </div>
           
         </div>
-        <div className='landing__overlay'></div>
-        {/* <img className='landing__img' style={{width: "100%"}} src={Pic} alt="" /> */}
+        {/* <div className='landing__overlay'></div> */}
+        {/* <img className='landing__img' src={Pic} alt="" /> */}
       </section>
       <Services />
       <Form orderClicked={orderClicked} />
