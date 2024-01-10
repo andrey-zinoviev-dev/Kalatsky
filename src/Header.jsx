3aa6c87b457c9bf55e2ef960f0c15c461a04817b
Andrey Zinoviev <dronis_dronis@mail.ru>
2024-01-10 12:01:22 +0300

    added container to header

--- src/Header.jsx
+++ src/Header.jsx
@@ -10,29 +10,31 @@ export default function Header({setNavOpened}) {
 
   return (
     <header className="header">
-      {/* <div className="container">
+      <div className="container">
+        <div className="header__wrapper">
+          <p id="logo">Адвокат Калацкий Борис Валерьевич</p>
+          <a href="tel:+79857626442">+7(985)7626442</a>
+          <button className="header__menu-btn" onClick={() => {
+                // navRef.current.classList.add("nav_opened");
+            setNavOpened(true);
+          }}>
+            <FontAwesomeIcon icon={faBars} />
+          </button>
+          <nav>
+            <ul>
+              {headerBtns.map((btn) => {
+                return <li key={btn.name}>
+                  <button>
+                    <a href={`#${btn.link}`}>{btn.name}</a>
+                  </button>
+                </li>
+              })}
+            </ul>
+          </nav>
+        </div>
 
-      </div> */}
-            <p id="logo">Адвокат Калацкий Борис Валерьевич</p>
-            <a href="tel:+79857626442">+7(985)7626442</a>
+      </div>
 
-            <button className="header__menu-btn" onClick={() => {
-              // navRef.current.classList.add("nav_opened");
-              setNavOpened(true);
-            }}>
-              <FontAwesomeIcon icon={faBars} />
-            </button>
-            <nav>
-              <ul>
-                {headerBtns.map((btn) => {
-                  return <li key={btn.name}>
-                    <button>
-                      <a href={`#${btn.link}`}>{btn.name}</a>
-                    </button>
-                  </li>
-                })}
-              </ul>
-            </nav>
     </header>
   )
 }
