dcd1179f2711261d588935c4deddc851885d4599
Andrey <sttrog_810@mail.ru>
2024-01-17 23:36:47 -0800

    added routing

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,23 @@ import React from 'react'
 import './App.css'
 import Landing from './Landing'
 import Header from './Header'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
 function App() {
   const [navOpened, setNavOpened] = React.useState(false);
-
+  const router = createBrowserRouter([
+    {path: "/", element: <Landing />},
+    {path: "/dev", element: <Header />}
+  ])
   return (
-    <>
-      <Header setNavOpened={setNavOpened}/>
-      <Landing navOpened={navOpened} setNavOpened={setNavOpened}/>
-    </>
+    <RouterProvider router={router} />
+    // <>
+    //   <Header setNavOpened={setNavOpened}/>
+    //   <Landing navOpened={navOpened} setNavOpened={setNavOpened}/>
+    // </>
   )
 }
 
